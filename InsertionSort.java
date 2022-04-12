4b75f8d38dd538c5539cf581947176dcf6299604
guilherme.estevao <guilherme.estevao@7comm.com.br>
2022-04-12 20:47:52 -0300

    update

--- InsertionSort.java
+++ InsertionSort.java
@@ -5,10 +5,45 @@ import java.util.Arrays;
 public class InsertionSort {
 	public static void main(String[] args) {
 		int[] A = { 5, 6, 9, 7, 1, 8 };
-		int[] orderA = insertionSort(A, A.length);	
-		System.out.println(Arrays.toString(orderA));	
+		int[] orderA = insertionSort(A, A.length);
+		System.out.println(Arrays.toString(orderA));
+
+		/*
+		 * Analise Insertion Sort
+		 * 
+		 * Pior caso
+		 * 
+		 * (n+1) (n+2) + 2n - 2 + 2n - 2 + 3n - 3 + 2n - 2 + 2n - 2 + 1 + 1 n² + 2n + n
+		 * + 2 + 2n - 2 + 2n - 2 + 3n -3 + 2n - 2 + 2n - 2 + 1 + 1 n² + 14n - 7
+		 * 
+		 * n² + 14n - 7 <= c * n² - 7 n² + 14n <= c * n² n²/n + 14n/n <= (c * n²)/n n +
+		 * 14 <= c * n 14 <= c * n - n 14 <= n(c - 1) 14 <= n(1) n = 14
+		 * 
+		 * --------------
+		 * 
+		 * n + 14 <= c * n 14 + 14 <= c * 14 c <= 28 / 14 c <= 2
+		 * 
+		 * Melhor caso
+		 * 
+		 * n² + 14n - 7 >= K * g(n) n² + 14n - 7 >= K * n - 7
+		 * 
+		 * n + 14 >= K * 1 14 >= K - n 14 => 1 - 1 14 => 0
+		 * 
+		 * K = 1 n = 1
+		 * 
+		 * Caso Medio
+		 * 
+		 * K * g(n) <= f(n) <= c * g(n) K * g(n) <= n² + 14n - 7 <= c * g(n)
+		 * 
+		 * K * n - 7 <= n² + 14n - 7 <= c * n² - 7 K * n <= n² + 14n <= c * n
+		 * 
+		 * (K * n)/n <= (n² + 14n)/n <= (c * n²)/n K <= n + 14 <= c * n K - n <= 14 <= c
+		 * * n - n K - n <= 14 <= n(c - 1) 1 - 14 <= 14 <= 14(2 - 1) -13 <= 14 <= 14
+		 * 
+		 * K = 1 c = 2 n = 14
+		 */
 	}
-	
+
 	public static int[] insertionSort(int[] A, int n) {
 		Integer pivo = null;
 		for (int i = 1; i <= (n - 1); i++) {
--- SelectionSort.java
+++ SelectionSort.java
@@ -7,6 +7,50 @@ public class SelectionSort {
 		int[] A = { 5, 6, 9, 7, 1, 8 };
 		int[] orderA = selectionSorte(A, A.length);
 		System.out.println(Arrays.toString(orderA));
+		/*
+		 * Analise Selection Sort
+		 * 
+		 * (n + 2) * 2n + n - 1 + 2n + 2 * (n + 2) + n + 2 + n + 2 + n + 2 + 1 2n² + 4n
+		 * + n - 1 + 2n + 2n + 4 + n + 2 + n + 2 + n + 2 + 1
+		 * 
+		 * f(n) = 2n² + 12n + 10
+		 * 
+		 * Pior caso
+		 * 
+		 * 2n² + 12n + 10 <= c * n² + 10 (corta os 10) 2n² + 12n <= c * n² 2n²/n + 12n/n
+		 * <= c * n²/n
+		 * 
+		 * 2n + 12 <= c * n
+		 * 
+		 * 12 <= c * n - 2n 12 <= n (c - 2) 12 <= n2 12/2 <= n
+		 * 
+		 * n = 6
+		 * 
+		 * --------------
+		 * 
+		 * 2n + 12 <= c * n 2 * 6 + 12 <= c * 12 24 <= c * 12 c <= 24 / 12 c <= 2
+		 * 
+		 * Melhor caso
+		 * 
+		 * 2n² + 12n + 10 >= K * g(n) 2n² + 12n + 10 >= K * n + 10
+		 * 
+		 * 2n² + 12n >= K * n 2n²/n + 12n/n >= (K * n)/n 2n + 12 >= K * 1 12 => K * 2n K
+		 * => 12 / 2 K => 6
+		 * 
+		 * n = 1 K = 6
+		 * 
+		 * Caso Medio
+		 * 
+		 * K * g(n) <= 2n² + 12n + 10 <= c * g(n) K * n + 10 <= 2n² + 12n + 10 <= c * n²
+		 * + 10
+		 * 
+		 * K * n <= 2n² + 12n <= c * n² (K * n)/n <= (2n² + 12n)/n <= (c * n²)/n
+		 * 
+		 * K <= 2n + 12 <= c * n - n K - 2n <= 12 <= n(c - 1) 6 - (2 * 6) <= 12 <=
+		 * 12(2-1) -6 <= 12 <= 12
+		 * 
+		 * K = 6 c = 2 n = 12
+		 */
 	}
 
 	public static int[] selectionSorte(int[] A, int n) {
